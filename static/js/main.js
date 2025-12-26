7fa173504efebe212d1294fef97947f6ab066d46
Gokul Mallabathula <gokulmallabathula@Gokuls-MacBook-Air.local>
2025-12-27 01:30:12 +0530

    Created and worked on FrontEnd designing

---
+++ static/js/main.js
@@ -0,0 +1,13 @@
+// Apple-style navbar behavior
+document.addEventListener("DOMContentLoaded", () => {
+    const navbar = document.querySelector(".navbar");
+  
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 10) {
+        navbar.classList.add("scrolled");
+      } else {
+        navbar.classList.remove("scrolled");
+      }
+    });
+  });
+  
